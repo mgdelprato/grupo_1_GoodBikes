b5a6cf37f4b7409f3d2e3b10a709ac5e6d3ccb4a
Damian Scorciapino <damian_scorciapino@hotmail.com>
2020-10-27 00:04:11 -0300

    modifico app.js y agrego maqueta de productCart

--- src/app.js
+++ src/app.js
@@ -8,13 +8,22 @@ const path = require('path');
 app.use( express.static( path.join(__dirname, './public') ) )
 
 app.get('/', function(req, res) {
-    res.sendFile( path.join(__dirname, './views/home.html') )
+    res.sendFile( path.join(__dirname, './views/index.html') )
 })
 
-app.get('/contacto', function(req, res) {
-    res.sendFile( path.join(__dirname, './views/contacto.html') )
+app.get('/login', function(req, res) {
+    res.sendFile( path.join(__dirname, './views/login.html') )
 })
 
+app.get('/productCart', function(req, res) {
+    res.sendFile( path.join(__dirname, './views/productCart.html') )
+})
+app.get('/productDetail', function(req, res) {
+    res.sendFile( path.join(__dirname, './views/productDetail.html') )
+})
+app.get('/register', function(req, res) {
+    res.sendFile( path.join(__dirname, './views/register.html') )
+})
 
 app.listen(3000, function() {
     console.log('Servidor corriendo en el puerto 3000')
