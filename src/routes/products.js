a61f27c36b90fde260c83ae53fdbb4a4c63643f6
Damian Scorciapino <damian_scorciapino@hotmail.com>
2020-12-06 01:46:43 -0300

    funcionalidad de register usuario y createProduct

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,17 +1,33 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
-
+const multer = require('multer');
 const productsController = require('../controllers/productsController');
+const path = require('path');
 
+//Multer para guardar las imagenes de productos
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/uploads/productos'));
+    },
+    filename: function (req, file, cb) {
+        console.log()
+        cb(null, req.body.producto + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+var upload = multer({ storage: storage })
 
 router.get('/productCart', productsController.carritoCompras);
 
 router.get('/productDetail', productsController.detalleProducto);
+// router.get('/productDetail/:id', productsController.detalleProducto); //Revisar esto a ver si está ok o directamente va el parametro
 
 router.get('/productCreate', productsController.crearProducto);
+router.post('/productCreate', upload.any(), productsController.grabarProducto);
 
-router.get('/productEdit', productsController.editarProducto);
+router.get('/:id/productEdit/', productsController.editarProducto);
+router.put('/:id/productEdit/', productsController.editarProducto);
+router.delete('/:id',productsController.borrarProducto);
 
 router.get('/productList', productsController.listarProducto);
 
