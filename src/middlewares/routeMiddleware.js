0debcf4562ea34ed1ea74978b1c193d0655eb952
Delprato Mauro Guido <MDELPRAT@tmoviles.com.ar>
2020-12-29 16:08:50 -0300

    Creación de Middle para redirección a profile en rutas /login /register

--- src/middlewares/routeMiddleware.js
+++ src/middlewares/routeMiddleware.js
@@ -1,13 +1,24 @@
 const path = require('path');
 
-function userLoggedIn(req, res, next){
-    console.log('estoy en el middle y vengo de '+ req.url + ' y en session tengo '+ req.session.user);
-    if(typeof req.session.user == 'undefined'){
-        res.render(path.join(__dirname, '../views/users/login'))
-    }else {
-        next()
+module.exports = {
+
+    userLoggedIn: function(req, res, next){
+        console.log('estoy en el middle y vengo de '+ req.url + ' y en session tengo '+ req.session.user);
+        if(typeof req.session.user == 'undefined'){
+            res.render(path.join(__dirname, '../views/users/login'))
+        }else {
+            next()    
+        }
+        
+    },
+    logInRegisterCase: function(req, res, next){
+        if(typeof req.session.user !== 'undefined'){
+            res.render(path.join(__dirname, '../views/users/profile'))
+        }else {
+            next()    
+        }
+        
+
     }
 
 }
-
-module.exports = {userLoggedIn};
