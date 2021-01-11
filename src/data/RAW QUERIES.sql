eb03a4184c74466764075e2d0c2e1abfa273cb32
Nahuel <nahuargentina@gmail.com>
2021-01-10 23:56:55 -0300

    DATABASE. Principales raw queries terminadas

--- src/data/RAW QUERIES.sql
+++ src/data/RAW QUERIES.sql
@@ -1,5 +1,7 @@
 USE goodbikesdb;
 
+SELECT * from PRODUCTS_IMAGES;
+
 -- <Trae datos para validar LOGIN y recuperar datos para session y cookies>
 	 SELECT id, first_name, email, password, avatar, is_admin 
      from USERS 
@@ -42,18 +44,19 @@ USE goodbikesdb;
 	Select PRODUCTS.id, PRODUCTS.category, PRODUCTS.title, PRODUCTS.brand, PRODUCTS.model, PRODUCTS.detail, PRODUCTS.price, PRODUCTS_IMAGES.image_name, PRODUCTS_IMAGES.principal_image 
     from PRODUCTS 
     LEFT JOIN PRODUCTS_IMAGES ON PRODUCTS.id = PRODUCTS_IMAGES.product_id_fk 
-    WHERE PRODUCTS.id = 16 ;
+    WHERE PRODUCTS.id = 16 and PRODUCTS.still_alive = 'YES';
    
 
 -- <Creación de usuario en register>
-	INSERT INTO Users(first_name,last_name,email,password,avatar)
-	VALUES ('Bruno','Diaz','Batman@gmail.com','$2a$12$1KfWvfIf8X4EugwY2pFyC.LEodu/c3y.I5wyiESTbzhV7k3dqwgHG', 'batman@gotica.com-1610312169591.PNG');
-
--- <Creación de Producto>
-	INSERT INTO Users(first_name,last_name,email,password,avatar)
+	INSERT INTO USERS(first_name,last_name,email,password,avatar)
 	VALUES ('Bruno','Diaz','Batman@gmail.com','$2a$12$1KfWvfIf8X4EugwY2pFyC.LEodu/c3y.I5wyiESTbzhV7k3dqwgHG', 'batman@gotica.com-1610312169591.PNG');
 
 
+
+
+    
+    
+    
 -- <EDITAR PROFILE>
 	-- <QUERY 1 editando datos de usuario>
     
@@ -93,10 +96,33 @@ USE goodbikesdb;
 
 
 -- <EDITAR PRODUCTOS>
-	-- <QUERY 1 editando datos de usuario>
+	-- <QUERY 1 edita datos de producto y cambia imagen principal>
     
-    UPDATE USERS 
-	SET first_name = 'Capitan', last_name = 'Buscapina', email = 'capitanbuscapina@patriciorey.com.ar',
-    password = 'Algo_re_hasheado' 
-    WHERE USERS.ID = 3;
+    UPDATE PRODUCTS
+    INNER JOIN PRODUCTS_IMAGES ON PRODUCTS_IMAGES.product_id_fk = PRODUCTS.id
     
+	SET PRODUCTS.category = 'Rodados', PRODUCTS.title = 'Playera', PRODUCTS.brand = 'Piola',
+    PRODUCTS.model = 'Playera Piola nunca Taxi', PRODUCTS.detail = 'Arranca, frena y dobla', PRODUCTS.price = 5000,
+    PRODUCTS.quantity = 10, PRODUCTS.offert = 'YES', PRODUCTS.has_price = 'YES', PRODUCTS.discount = 10,
+    PRODUCTS_IMAGES.image_name = 'una_imagen.JPG'
+    
+    WHERE PRODUCTS.id = 3 and PRODUCTS_IMAGES.principal_image = 'YES'; 
+    
+    
+    -- <QUERY 2 Borrado logico de productos>
+    UPDATE PRODUCTS
+    SET still_alive = 'NO'
+    WHERE PRODUCTS.id = 3;
+    
+    
+-- <CREAR PRODUCTO en 2 pasos>
+
+	-- <Inserta datos de producto>
+	INSERT INTO PRODUCTS(Category,Title,Brand,Model,Detail,Price,quantity) 
+	VALUES 
+	('Indumentaria','Casquito Piola2','Casquito Don Roberto','Don Roberto con aeroventilas',
+	'Casquito Piola Don Roberto con aeroventilas',100,3000);
+
+	-- <TRAE el id asignado al producto recién ingresado e inserta la imagen> 
+	INSERT INTO PRODUCTS_IMAGES (product_id_fk,image_name,principal_image)
+    VALUES ((SELECT MAX(id) FROM PRODUCTS),'un_producto_fachero2.JPG','YES');
