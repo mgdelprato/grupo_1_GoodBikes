4571cba1004db172faa5cff5b0d51b5762100dac
Damian Scorciapino <damian_scorciapino@hotmail.com>
2021-01-16 01:33:42 -0300

    CRUD productos, pendientes vistas e imagenes

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,19 +4,6 @@ const { validationResult } = require('express-validator');
 let db = require('../data/models')
 
 
-//Leo JSON de productos y lo parseo
-let productos = fs.readFileSync(path.join(__dirname,'../data/products.json'),'utf-8');
-productos = JSON.parse(productos);
-
-
-
-//Función para obtener ID de producto autoincremental
-let ultimoId = 0
-for(let i=0; i<productos.length ; i++){
-    if(ultimoId<productos[i].ID){
-        ultimoId=productos[i].ID;
-    }
-}
 
 /* CONTROLLER QUE CONTIENE LA LÓGICA DE NEGOCIO RELACIONADA A PRODUCTOS */
 
@@ -64,8 +51,7 @@ let productsController = {
                         .then(function(imagen){
                             console.log("inserto " + imagen)
                         })
-                        res.render(path.join(__dirname,'../views/products/productList'),{productos:productos}) 
-                    
+                        res.redirect('/admin/products/productList')                    
                 }
             })
         }else{
@@ -79,58 +65,40 @@ let productsController = {
     editarProducto: function(req, res) {
 
         //Busco el producto a editar
-        let productoEditar = req.params.id;
-        let posicionArrayProductos
-        for(let i=0; i<productos.length;i++){
-            if(productoEditar == productos[i].ID){
-                
-                posicionArrayProductos = i 
-
-            } 
-        }
+        db.Product.findByPk(req.params.id)
+        .then(function(productoEditar){
+            
+            //Renderizo la vista enviandole los valores del producto a editar para utilizarlos en la vista
+            res.render(path.join(__dirname, '../views/products/productEdit.ejs'),{productoEditar:productoEditar})
+        })
   
-        productoEditar = productos[posicionArrayProductos];
-        //Renderizo la vista enviandole los valores del producto a editar para utilizarlos en la vista
-        res.render(path.join(__dirname, '../views/products/productEdit.ejs'),{productoEditar:productoEditar})
-
+        //*******************PENDIENTE MANEJO DE IMAGENES*****************
     },
     // Método (asociado al PUT del admin) para guardar la edición realizada sobre un producto
     actualizarProducto: function(req,res){
-        let productoEditar = parseInt(req.params.id); 
         let error  = validationResult(req);
-        let arrayImagen =[];
-     
-        //Función para cargar mas de una imágen al producto
-        const insertarImagen  = ()=>{
-            for(let i=0; i<req.files.length; i++){
-                arrayImagen.push(req.files[i].filename)
-         
-            }
-        }
-        insertarImagen();    
+      
         //Si no hay errores guardo los valores del producto editado, y renderizo la vista de listado de productos
         if(error.isEmpty()){
-        for (let i=0; i<productos.length;i++){
-            if(productoEditar==productos[i].ID){
-                productos[i] = {
-                    ID: productoEditar,
-                    Categoria: req.body.categoria,
-                    Titulo: req.body.producto,
-                    Marca: req.body.marca,
-                    Modelo: req.body.modelo,
-                    Detalles: req.body.detalle,
-                    Imagen:arrayImagen,
-                    Precio: req.body.precio,
-                    Cantidad: req.body.cantidad 
-
+       
+            db.Product.update({
+                category: req.body.categoria,
+                title: req.body.producto,
+                brand: req.body.marca,
+                model: req.body.modelo,
+                detail: req.body.detalle,
+                price: req.body.precio,
+                quantity: req.body.cantidad,
+            },{
+                where:{
+                    id:req.params.id
                 }
-              
-            }
-        }
-           
-
-            fs.writeFileSync(path.join(__dirname,'../data/products.json'),JSON.stringify(productos,null,4))
-            res.render(path.join(__dirname,'../views/products/productList'), {productos:productos} )
+            })
+            .then(function(){
+                
+                res.redirect('/admin/products/productList')
+            })
+       //*******************************PENDIENTE ANALISIS UPDATE IMAGENES******************************
         }else{
             // Si hay errores, los mapeo y renderizo la vista de edición d eproducto  mostrando los errores
             return res.render(path.join(__dirname,'../views/products/productEdit/:id'), {
@@ -141,7 +109,15 @@ let productsController = {
     //Método (asociado a GET en el admin) para renderizar la vista de listado de productos
     listarProducto: function(req, res) {
         
-        res.render( path.join(__dirname, '../views/products/productList.ejs'),{productos:productos} )
+        db.Product.findAll({
+            where:{
+                still_alive:'YES'
+            }
+        })
+        .then(function(productos){
+
+            res.render( path.join(__dirname, '../views/products/productList.ejs'),{productos:productos} )
+        })
         
     },
    //Método (asociado al DELETE) para borrar un producto en particular 
@@ -151,12 +127,17 @@ let productsController = {
        //Chequeo que no hay errores, si está OK, recupero el producto que hay que eliminiar y lo filtro del array
         if(error.isEmpty()){
             let productoEliminar = req.params.id;
-                
-                productos = productos.filter(productos=>productos.ID!=productoEliminar)   
-               
+                db.Product.update({
+                    still_alive:'NO'
+                },{
+                    where:{
+                        id:req.params.id
+                    }
+                })
+                .then(function(){
 
-                fs.writeFileSync(path.join(__dirname,'../data/products.json'),JSON.stringify(productos,null,4))
-                res.redirect('/admin/products/productList')
+                    res.redirect('/admin/products/productList')
+                })
 
         }else{
             //Si hay errores, los mapeo y renderizo la vista de listado de productos
@@ -172,11 +153,17 @@ let productsController = {
     },
     //Método (asociado al GET de products) para renderizar la vista de los productos de una categoria en particular
     buscarProducto: function(req,res){
-        //Obtengo la categoria
-        let categoria = req.params.categoria
-        //Obtengo los productos que pertenecen a la categoria y renderizo la vista con esos productos 
-        productosCategorizados = productos.filter(productos=>productos.Categoria==categoria)
-        res.render( path.join(__dirname, '../views/products/productSearch.ejs'),{productosCategorizados:productosCategorizados})
+
+        db.Product.findAll({
+            where:{
+                category:req.params.categoria
+            }
+        })
+        .then(function(productosCategorizados){
+            
+            res.render( path.join(__dirname, '../views/products/productSearch.ejs'),{productosCategorizados:productosCategorizados})
+        })
+ 
     }
 
 }
