ce2524bccfdf111cbba2b24ce86b2f89dfe201eb
Nahu <nahuargentina@gmail.com>
2021-02-08 22:51:21 -0300

    Inicios del JS front en el header

---
+++ public/js/header.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("Lista_Nav");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+    }
+    
