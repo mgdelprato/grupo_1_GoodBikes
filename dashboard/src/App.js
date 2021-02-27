b3b45f8a4f7482badd75544c6c1846fd96f690f6
Nahu <nahuargentina@gmail.com>
2021-02-26 23:27:31 -0300

    React: assets incluidas en public

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -106,7 +106,7 @@ function App() {
                       <li className ="nav-item dropdown no-arrow">
                         <a className ="nav-link dropdown-toggle" href="/" id="userDropdown">
                           <span className ="mr-2 d-none d-lg-inline text-gray-600 small">Walter White</span>
-                          <img className ="img-profile rounded-circle" src="assets/images/dummy-avatar.jpg" width="60"/>
+                          <img className ="img-profile rounded-circle" src={'assets/images/dummy-avatar.jpg'} width="60"/>
                         </a>
                       </li>
 
