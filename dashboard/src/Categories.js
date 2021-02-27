dbd2a7ece0d62697533d7bce210a235376d38022
Nahu <nahuargentina@gmail.com>
2021-02-27 00:42:14 -0300

    React: Categories modularizado en static

---
+++ dashboard/src/Categories.js
@@ -0,0 +1,54 @@
+import React from   'react';
+
+function Categories(){
+
+    return(
+        <div className ="row">
+        <div className ="col-lg-6 mb-4">
+          <div className ="card bg-info text-white shadow">
+            <div className ="card-body">
+              Category 01
+            </div>
+          </div>
+        </div>
+        <div className ="col-lg-6 mb-4">
+          <div className ="card bg-info text-white shadow">
+            <div className ="card-body">
+              Category 02
+            </div>
+          </div>
+        </div>
+        <div className ="col-lg-6 mb-4">
+          <div className ="card bg-info text-white shadow">
+            <div className ="card-body">
+              Category 03
+            </div>
+          </div>
+        </div>
+        <div className ="col-lg-6 mb-4">
+          <div className ="card bg-info text-white shadow">
+            <div className ="card-body">
+              Category 04
+            </div>
+          </div>
+        </div>
+        <div className ="col-lg-6 mb-4">
+          <div className ="card bg-info text-white shadow">
+            <div className ="card-body">
+              Category 05
+            </div>
+          </div>
+        </div>
+        <div className ="col-lg-6 mb-4">
+          <div className ="card bg-info text-white shadow">
+            <div className ="card-body">
+              Category 06
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    )
+}
+
+export default Categories
--- dashboard/src/Measures.js
+++ dashboard/src/Measures.js
@@ -1,4 +1,3 @@
-import { render } from 'ejs';
 import React from 'react';
 
 function Measures(){
