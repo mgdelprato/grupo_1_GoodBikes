1cf5eb77864cb2d711874b4f78e853ee060cfa02
Nahu <nahuargentina@gmail.com>
2021-03-02 20:58:20 -0300

    React: Enlaces en las categorías insertadas

--- dashboard/src/Categories.js
+++ dashboard/src/Categories.js
@@ -1,11 +1,11 @@
 import React from   'react';
 
 let categoriesDetails = [
-  {text: 'Rodados'},
-  {text: 'Equipamiento'},
-  {text: 'Indumentaria'},
-  {text: 'Accesorios'},
-  {text: 'Taller'}
+  {text: 'Rodados', link:'http://localhost:5000/products/productSearch/Rodados'},
+  {text: 'Equipamiento', link:'http://localhost:5000/products/productSearch/Equipamiento'},
+  {text: 'Indumentaria', link:'http://localhost:5000/products/productSearch/Indumentaria'},
+  {text: 'Accesorios', link:'http://localhost:5000/products/productSearch/Accesorios'},
+  {text: 'Taller', link:'http://localhost:5000/products/productSearch/Taller'}
 ]
 
 function Categories(props){
@@ -15,7 +15,7 @@ function Categories(props){
         <div className ="col-lg-6 mb-4" key ={n}>
           <div className ="card bg-info text-white shadow">
             <div className ="card-body" >
-              {item.text } 
+               <a href={item.link}>{item.text}</a> 
             </div>
           </div>
         </div>
