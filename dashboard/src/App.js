989f898a89f5993c5135fb13e2936246249feb69
Nahu <nahuargentina@gmail.com>
2021-03-03 00:26:09 -0300

    React: Componente HeaderItems generado

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -3,6 +3,7 @@ import Categories from './Categories';
 import Measures from './Measures';
 import Container from './Container';
 import NavItems from './NavItems';
+import HeaderItems from './NavItems';
 
 
 
@@ -10,22 +11,21 @@ import NavItems from './NavItems';
 function App() {
   return (
 
-              <div id="wrapper">
+        <div id="wrapper">
 
-              <ul className ="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+                  <ul className ="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
                   
                   <a className ="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-                    <div className ="sidebar-brand-icon">
-                      <i className ="fas fa-chart-line"></i>
-                    </div>
+                    <div className ="sidebar-brand-icon"><i className ="fas fa-chart-line"></i></div>
                     <div className ="sidebar-brand-text mx-3">Admin</div>
                   </a>
 
-                
-                  <hr className ="sidebar-divider my-0"/>              
+                  <hr className ="sidebar-divider my-0"/>      
+
                   <NavItems classOne="nav-item active" classTwo="nav-link" classThree="fas fa-fw fa-tachometer-alt" text = "Dashboard" />
-                        <hr className ="sidebar-divider"/>
-                        <div className ="sidebar-heading">Actions</div>
+                        
+                  <hr className ="sidebar-divider"/>
+                  <div className ="sidebar-heading">Actions</div>
 
                   <NavItems classOne="nav-item" classTwo="nav-link collapsed" classThree="fas fa-fw fa-folder" text = "Pages" />
                   <NavItems classOne="nav-item" classTwo="nav-link" classThree="fas fa-fw fa-chart-area" text = "Charts" />                
@@ -39,44 +39,27 @@ function App() {
                 
               <div id="content">
                  
-              <nav className ="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                  <nav className ="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-                    <button id="sidebarToggleTop" className ="btn btn-link d-md-none rounded-circle mr-3">
-                      <i className ="fa fa-bars"></i>
-                    </button>
+                        <button id="sidebarToggleTop" className ="btn btn-link d-md-none rounded-circle mr-3">
+                          <i className ="fa fa-bars"></i>
+                        </button>
 
-                    
-                    <ul className ="navbar-nav ml-auto">
+                        
+                        <ul className ="navbar-nav ml-auto">
 
-                    
-                      <li className ="nav-item dropdown no-arrow mx-1">
-                        <a className ="nav-link dropdown-toggle" href="/" id="alertsDropdown">
-                          <i className ="fas fa-bell fa-fw"></i>
-                          
-                          <span className ="badge badge-danger badge-counter">3+</span>
-                        </a>
-                      </li>
+                          <HeaderItems/>
 
-                      
-                      <li className ="nav-item dropdown no-arrow mx-1">
-                        <a className ="nav-link dropdown-toggle" href="/" id="messagesDropdown">
-                          <i className ="fas fa-envelope fa-fw"></i>
-                          
-                          <span className ="badge badge-danger badge-counter">7</span>
-                        </a>
-                      </li>
+                          <div className ="topbar-divider d-none d-sm-block"></div>
 
-                      <div className ="topbar-divider d-none d-sm-block"></div>
+                            <li className ="nav-item dropdown no-arrow">
+                              <a className ="nav-link dropdown-toggle" href="/" id="userDropdown">
+                                <span className ="mr-2 d-none d-lg-inline text-gray-600 small">Walter White</span>
+                                <img className ="img-profile rounded-circle" src={'assets/images/dummy-avatar.jpg'} width="60"/>
+                              </a>
+                            </li>
 
-                      
-                      <li className ="nav-item dropdown no-arrow">
-                        <a className ="nav-link dropdown-toggle" href="/" id="userDropdown">
-                          <span className ="mr-2 d-none d-lg-inline text-gray-600 small">Walter White</span>
-                          <img className ="img-profile rounded-circle" src={'assets/images/dummy-avatar.jpg'} width="60"/>
-                        </a>
-                      </li>
-
-                    </ul>
+                        </ul>
 
                 </nav>
                   
@@ -124,16 +107,6 @@ function App() {
               
 
             </div>
-
-
-
-
-
-
-
-
-
-
 
   );
 }
---
+++ dashboard/src/HeaderItems.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+let HeaderItemsDetails = 
+[{
+  id: 'alertsDropdown',
+  text: '3+',
+  icon: 'fas fa-bell fa-fw'
+},
+{
+  id: 'messagesDropdown',
+  text: '7',
+  icon: 'fas fa-envelope fa-fw'
+}
+]
+
+function HeaderItems(props){
+    return(
+      HeaderItemsDetails.map((item,n) =>
+    
+    <li className ="nav-item dropdown no-arrow mx-1" key ={n}>
+      <a className ="nav-link dropdown-toggle" href="/" id={item.id}>
+        <i className ={item.icon}></i>
+        <span className ="badge badge-danger badge-counter">{item.text}</span>
+      </a>
+    </li>
+     
+    ))
+}
+
+export default HeaderItems
