0a5ca30c56dd8a8af895676b3e861f2b89013b83
Nahu <nahuargentina@gmail.com>
2021-03-02 23:59:38 -0300

    React: Componente NavItems Generado

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Categories from './Categories';
 import Measures from './Measures';
 import Container from './Container';
+import NavItems from './NavItems';
+
+
 
 
 function App() {
@@ -9,68 +12,35 @@ function App() {
 
               <div id="wrapper">
 
-              
               <ul className ="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-                
-                <a className ="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-                  <div className ="sidebar-brand-icon">
-                    <i className ="fas fa-chart-line"></i>
-                  </div>
-                  <div className ="sidebar-brand-text mx-3">Admin</div>
-                </a>
-
-                
-                <hr className ="sidebar-divider my-0"/>
-
-                
-                <li className ="nav-item active">
-                  <a className ="nav-link" href="/">
-                    <i className ="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-                </li>
-
-                
-                <hr className ="sidebar-divider"/>
-
-                
-                <div className ="sidebar-heading">Actions</div>
-
-                
-                <li className ="nav-item">
-                  <a className ="nav-link collapsed" href="/">
-                    <i className ="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
+                  
+                  <a className ="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+                    <div className ="sidebar-brand-icon">
+                      <i className ="fas fa-chart-line"></i>
+                    </div>
+                    <div className ="sidebar-brand-text mx-3">Admin</div>
                   </a>
-                </li>
 
                 
-                <li className ="nav-item">
-                  <a className ="nav-link" href="/">
-                    <i className ="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-                </li>
+                  <hr className ="sidebar-divider my-0"/>              
+                  <NavItems classOne="nav-item active" classTwo="nav-link" classThree="fas fa-fw fa-tachometer-alt" text = "Dashboard" />
+                        <hr className ="sidebar-divider"/>
+                        <div className ="sidebar-heading">Actions</div>
 
-                
-                <li className ="nav-item">
-                  <a className ="nav-link" href="/">
-                    <i className ="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
-                </li>
+                  <NavItems classOne="nav-item" classTwo="nav-link collapsed" classThree="fas fa-fw fa-folder" text = "Pages" />
+                  <NavItems classOne="nav-item" classTwo="nav-link" classThree="fas fa-fw fa-chart-area" text = "Charts" />                
+                  <NavItems classOne="nav-item" classTwo="nav-link" classThree="fas fa-fw fa-table" text = "Tables" />                
 
-                
-                <hr className ="sidebar-divider d-none d-md-block"/>
+                  <hr className ="sidebar-divider d-none d-md-block"/>
               </ul>
               
               <div id="content-wrapper" className ="d-flex flex-column">
 
                 
-                <div id="content">
+              <div id="content">
+                 
+              <nav className ="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-                  
-                  <nav className ="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    
                     <button id="sidebarToggleTop" className ="btn btn-link d-md-none rounded-circle mr-3">
                       <i className ="fa fa-bars"></i>
                     </button>
@@ -108,29 +78,32 @@ function App() {
 
                     </ul>
 
-                  </nav>
+                </nav>
                   
 
                   
                   <div className ="container-fluid">
 
-                    
                     <div className ="d-sm-flex align-items-center justify-content-between mb-4">
                       <h1 className ="h3 mb-0 text-gray-800">App Dashboard</h1>
                     </div>
-
-                    
-                    
-                  
+ 
                     <div className ="row">
                       <Measures/>
                     </div>
 
      
                     <div className ="row">
-
-                      <Container text = "Last product in Data Dase" classDetails = "card-body"/>
-                      <Container text = "Categories in Data Base" classDetails = "row" />
+                      <Container text = "Last product in Data Dase" classDetails = "card-body">
+                                      <div className ="text-center">
+                                      <img className ="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 25 + 'rem'}} src="assets/images/product_dummy.svg" alt="image dummy"/>
+                                      </div>
+                                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
+                                      <a target="_blank" rel="nofollow" href="/">View product detail</a>
+                      </Container>
+                      <Container text = "Categories in Data Base" classDetails = "row">
+                                      <Categories/>
+                      </Container>
                     </div>
 
                   </div>
