5326898b97a96d43d9cf573b60c7028218592d32
Nahu <nahuargentina@gmail.com>
2021-03-09 23:47:35 -0300

    Carrito: ID recuperados en storage para front

--- public/js/productCart.js
+++ public/js/productCart.js
@@ -1,7 +1,26 @@
-const Suma = document.getElementById('suma');
+let elemento = document.getElementsByClassName('elItem'); 
+let cantidad = elemento.length;
+let array_id = Array();
+
+for(let i = 0; i < cantidad; i++){
+    let id = elemento[i].getAttribute('id');
+
+    array_id.push('id:' + id);
+}
+
+console.log(array_id);
+
+
+
+// Guardo el objeto como un string
+localStorage.setItem('datos', JSON.stringify(array_id));
+
+
+console.log('Dato recuperado para Front' + localStorage.getItem('datos'))
+
 
 // document.getElementById('2') + document.getElementById('14')
 
 
 
-// Suma.innerHTML = "ATR PERRO CUMBIA"
+// Suma.innerHTML = ""
