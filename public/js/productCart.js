435f2fb7739c01fd134d8632caa5dcb150935f30
Nahu <nahuargentina@gmail.com>
2021-03-10 00:23:27 -0300

    Precio, cantidad y subtotal accesibles desde localstorage

--- public/js/productCart.js
+++ public/js/productCart.js
@@ -1,22 +1,28 @@
 let elemento = document.getElementsByClassName('elItem'); 
-let cantidad = elemento.length;
 let array_id = Array();
 
-for(let i = 0; i < cantidad; i++){
+for(let i = 0; i < elemento.length; i++){
     let id = elemento[i].getAttribute('id');
 
-    array_id.push('id:' + id);
+    array_id.push(id);
 }
 
 console.log(array_id);
 
 
 
-// Guardo el objeto como un string
+// Guardo el objeto como un string en Storage
 localStorage.setItem('datos', JSON.stringify(array_id));
 
+//Recuperar valores de Storage
+let items = JSON.parse(localStorage.getItem('datos'))
 
-console.log('Dato recuperado para Front' + localStorage.getItem('datos'))
+//Accionando sobre valores recuperados
+items.map(item => 
+    
+    alert('Producto ' + item + '\n Precio: ' + document.getElementById('P'+item).textContent + 'Cantidad: ' + 
+    document.getElementById('Q'+item).value + 'Subtotal: ' + parseInt(document.getElementById('P'+item).textContent,10)*parseInt(document.getElementById('Q'+item).value,10)            
+            )) 
 
 
 // document.getElementById('2') + document.getElementById('14')
