51e92b94cce0e9f823c99be4f2d41ad173d83408
Nahu <nahuargentina@gmail.com>
2021-03-11 00:01:52 -0300

    JS Front. Carrito modularizado en funciones

--- public/js/productCart.js
+++ public/js/productCart.js
@@ -1,48 +1,48 @@
-let elemento = document.getElementsByClassName('elItem'); 
-let array_id = Array();
 
-for(let i = 0; i < elemento.length; i++){
-    let id = elemento[i].getAttribute('id');
+function levantaID(){
 
-    array_id.push(id);
-}
+        let elemento = document.getElementsByClassName('elItem'); 
+        let array_id = Array();
 
-console.log(array_id);
+        for(let i = 0; i < elemento.length; i++){
+            let id = elemento[i].getAttribute('id');
+            array_id.push(id);
+        }
+
+        console.log(array_id);
+        localStorage.setItem('datos', JSON.stringify(array_id)); // Guardo el objeto como un string en Storage
+    }
 
 
 
-// Guardo el objeto como un string en Storage
-localStorage.setItem('datos', JSON.stringify(array_id));
 
-//Recuperar valores de Storage
-let items = JSON.parse(localStorage.getItem('datos'))
 
-//Accionando sobre valores recuperados
-items.map(item => 
-    
-        document.getElementById('PxQ'+item).innerHTML = 
 
-        '<p id="PxQ"' + item + 'class="valorProducto">' +  
-        parseInt(document.getElementById('P'+item).textContent,10)*parseInt(document.getElementById('Q'+item).value,10)
-        + '</p>'
-                   
-)
+function recalcula(){
+    let items = JSON.parse(localStorage.getItem('datos'))
 
-let acumula = 0
-let valor
+    //Calcula subtotales
+    items.map(item => 
+            document.getElementById('PxQ'+item).innerHTML = 
+            '<p id="PxQ"' + item + 'class="valorProducto">' +  
+            parseInt(document.getElementById('P'+item).textContent,10)*parseInt(document.getElementById('Q'+item).value,10)
+            + '</p>'
+            )
+    //Calcula totales
+    let acumula = 0
+    let valor
 
-for (let i = 0; i < items.length; i++) {
-    valor = parseInt(document.getElementById('PxQ'+items[i]).textContent,10)
-    acumula = acumula + valor
-    
-}
+    for (let i = 0; i < items.length; i++) {
+        valor = parseInt(document.getElementById('PxQ'+items[i]).textContent,10)
+        acumula = acumula + valor        
+        }
 
-document.getElementById('suma').innerHTML = '<p id="suma" class="precio">' + acumula + '</p>'
-    
-   
+    document.getElementById('suma').innerHTML = '<p id="suma" class="precio">' + acumula + '</p>'
         
-    
-
+}
+        
+levantaID()
+recalcula()
                
 
 
