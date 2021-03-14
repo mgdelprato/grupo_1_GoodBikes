d9a2c3131fc577221191b349a5b89d0214729c90
Nahu <nahuargentina@gmail.com>
2021-03-14 16:09:11 -0300

    Dashboard CSS adaptado al proyecto

--- dashboard/src/assets/components/TheFoot.js
+++ dashboard/src/assets/components/TheFoot.js
@@ -7,7 +7,7 @@ function TheFoot(props){
     <footer className ="sticky-footer bg-white">
       <div className ="container my-auto">
         <div className ="copyright text-center my-auto">
-          <span>Copyright &copy; Dashboard 2020</span>
+          <span>Goobikes &copy; Dashboard 2021</span>
         </div>
       </div>
     </footer>
